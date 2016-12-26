98895b5095b6b58b679443846fd310fc1a893e7c
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2016-12-26 03:07:08 -0200

    Exercicios
    Exercicios feitos dia 26/12/2016

---
+++ Exercicio 2/Exercicio 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Victor Hugo");
+            Console.ReadLine();
+        }
+    }
+}
